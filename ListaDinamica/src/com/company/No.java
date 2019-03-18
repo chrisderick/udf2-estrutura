729fb755e5fc1fab028ca831f91d73ec09cd44d2
Christhyan Derick <christhyanderick@gmail.com>
2019-03-18 11:15:24 -0300

    UDF 18/03

---
+++ ListaDinamica/src/com/company/No.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class No {
+    private String elemento;
+    private No proximo;
+
+    public No(String elemento) {
+        this.elemento = elemento;
+    }
+
+    public String getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(String elemento) {
+        this.elemento = elemento;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+}
