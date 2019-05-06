4004269e7852ba3747e7ad956836246a11bc9ff1
chrisderick <christhyanderick@gmail.com>
2019-05-06 17:17:45 -0300

    Adição das últimas saídas possíveis

--- ProjetoED/src/AdivinhaEstrutura.java
+++ ProjetoED/src/AdivinhaEstrutura.java
@@ -1,12 +1,16 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//TODO: Arrumar quando tem entradas e saídas misturadas visto que o programa nao reconhece;
+//TODO: Classificar como impossível quando um valor é removido do meio da estrutura;
+
 public class AdivinhaEstrutura {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         Integer tamanho = s.nextInt();
-        Integer[] vetor = new Integer[tamanho];
+        //Integer[] vetor = new Integer[tamanho];
         ArrayList<Integer> insercoes = new ArrayList<>();
         ArrayList<Integer> remocoes = new ArrayList<>();
         Integer tipoEstrutura = null;
@@ -14,29 +18,33 @@ public class AdivinhaEstrutura {
         //Garante que os valores removidos existem na estrutura
         boolean existe = true;
 
-        //Permite a entrada de comandos na quantidade informada anteriormente
-        for(int i = 0 ; i < vetor.length ; i++) {
-            s = new Scanner(System.in);
-            String linha = s.nextLine();
+        if(tamanho >= 1 && tamanho <= 1000) {
+            //Permite a entrada de comandos na quantidade informada anteriormente
+            for (int i = 0; i < tamanho; i++) {
+                s = new Scanner(System.in);
+                String linha = s.nextLine();
 
-            //TODO: Confirmar com a professora se é pra colocar impossível para estas situações
-            //Confirma que um valor foi informado
-            if(linha.length() > 2) {
-                //Armazena o comando (inserção(1)/remoção(2)) a ser executado no valor informado;
-                Integer valor = Integer.parseInt(linha.substring(2));
-                Integer comando = Integer.parseInt(String.valueOf(linha.charAt(0)));
+                //Confirma que um valor foi informado
+                if (linha.length() > 2) {
+                    //Armazena o comando (inserção(1)/remoção(2)) a ser executado no valor informado;
+                    Integer valor = Integer.parseInt(linha.substring(2));
+                    Integer comando = Integer.parseInt(String.valueOf(linha.charAt(0)));
 
-                //Verifica se o valor está nos conformes estabelecidos pelo enunciado
-                if(valor >= 0 && valor <= 100){
-                    //Verifica se está sendo realizada uma inserção ou uma remoção na estrutura
-                    if(comando == 1){
-                        //vetor[i] = valor;
-                        if(valor > maiorValor) maiorValor = valor;
-                        insercoes.add(valor);
-                    } else if (comando == 2) {
-                        //Confere se o valor a ser removido já foi inserido antes
-                        if (insercoes.contains(valor)){
-                            remocoes.add(valor);
+                    //Verifica se o valor está nos conformes estabelecidos pelo enunciado
+                    if (valor >= 0 && valor <= 100) {
+                        //Verifica se está sendo realizada uma inserção ou uma remoção na estrutura
+                        if (comando == 1) {
+                            //vetor[i] = valor;
+                            if (valor > maiorValor) maiorValor = valor;
+                            insercoes.add(valor);
+                        } else if (comando == 2) {
+                            //Confere se o valor a ser removido já foi inserido antes
+                            if (insercoes.contains(valor)) {
+                                remocoes.add(valor);
+                            } else {
+                                tipoEstrutura = 0;
+                                break;
+                            }
                         } else {
                             tipoEstrutura = 0;
                             break;
@@ -49,16 +57,15 @@ public class AdivinhaEstrutura {
                     tipoEstrutura = 0;
                     break;
                 }
-            } else {
-                tipoEstrutura = 0;
-                break;
             }
+        } else {
+            tipoEstrutura = 1;
         }
 
         //Comparações
         //Tipo de Estrutura 1: Not Sure
-        if (tipoEstrutura != null) {
-            if (tamanho == 1) {
+        if (tipoEstrutura == null) {
+            if (tamanho == 1 || tamanho == 2) {
                 tipoEstrutura = 1;
             } else if (tamanho > 3 && insercoes.size() == remocoes.size()) {
                 //Confera se as listas estão invertidas
@@ -108,7 +115,9 @@ public class AdivinhaEstrutura {
                     if (estaOrdenada) {
                         tipoEstrutura = 1;
                     }
-                } else if (estaoIguais) {
+                }
+                //Estrutura é Fila normal ou de Prioridade
+                else if (estaoIguais) {
                     //Confera se as inserções estão em ordem decrescente
                     Integer anterior = insercoes.get(0);
                     for (Integer valor : insercoes) {
@@ -119,7 +128,7 @@ public class AdivinhaEstrutura {
                         }
                         anterior = valor;
                     }
-                    //Confera se as remoções estão em ordem decrescente
+                    //Confere se as remoções estão em ordem decrescente
                     anterior = remocoes.get(0);
                     for (Integer valor : remocoes) {
                         //Não considerando valores iguais
@@ -133,8 +142,105 @@ public class AdivinhaEstrutura {
                         tipoEstrutura = 1;
                     }
                 }
-            } else if (remocoes.get(remocoes.size() - 1).equals(maiorValor)) {
-                //TODO: Condição G - NOT SURE, tinha um "estaOrdenada" na comparação desse if
+            }
+            else if (remocoes.get(remocoes.size() - 1).equals(maiorValor)) {
+
+            }
+        }
+
+        //Tipo de Estrutura 2: Pilha (stack)
+        if (tipoEstrutura == null) {
+            //São necessários no mínimo 4 comandos para determinar se a estrutura é uma pilha
+            if (tamanho > 3 && !remocoes.isEmpty()){
+                boolean pilha = true;
+                int fimInsercoes = insercoes.size()-1;
+                int inicioRemocoes = 0;
+                //Verifica se as remoções feitas estão no padrão de pilha (LIFO)
+                for (int i = remocoes.size()-1; i >= 0; i--){
+                    if (!remocoes.get(inicioRemocoes).equals(insercoes.get(fimInsercoes))){
+                        pilha = false;
+                        break;
+                    }
+                    inicioRemocoes++;
+                    fimInsercoes--;
+                }
+                if (pilha){
+                    tipoEstrutura = 2;
+                }
+            }
+        }
+
+        //Tipo de Estrutura 3: Fila (queue)
+        if (tipoEstrutura == null) {
+            if (tamanho > 2 && !remocoes.isEmpty()) {
+                boolean fila = true;
+                int inicioInsercoes = 0;
+                int inicioRemocoes = 0;
+                //Verifica se as remoções feitas estão no padrão de fila (FIFO)
+                for (int i = remocoes.size() - 1; i >= 0; i--) {
+                    if (!remocoes.get(inicioRemocoes).equals(insercoes.get(inicioInsercoes))) {
+                        fila = false;
+                        break;
+                    }
+                    inicioInsercoes++;
+                    inicioRemocoes++;
+                }
+                if (fila) {
+                    tipoEstrutura = 3;
+                }
+            }
+        }
+
+        //Tipo de Estrutura 4: Fila de Prioridade (priority queue)
+        if (tipoEstrutura == null) {
+            //O primeiro valor a ser removido deve ser o maior dos valores inseridos
+            if (tamanho > 3 && !remocoes.isEmpty() && remocoes.get(0).equals(maiorValor)) {
+                boolean filapri = true;
+                /*int inicioInsercoes = 0;
+                int inicioRemocoes = 0;
+                //Verifica se as remoções feitas estão no padrão de fila de prioridade (Remoção dos maiores valores em ordem)
+                for (int i = remocoes.size() - 1; i >= 0; i--) {
+                    if (!remocoes.get(inicioRemocoes).equals(insercoes.get(inicioInsercoes))) {
+                        filapri = false;
+                        break;
+                    }
+                    inicioInsercoes++;
+                    inicioRemocoes++;
+                }
+                if (filapri) {
+                    tipoEstrutura = 4;
+                }*/
+                if (filapri) {
+                    tipoEstrutura = 4;
+                }
+            }
+        }
+
+        //Se nenhuma das comparações definir o valor de tipoEstrutura, esta a define como not sure
+        if (tipoEstrutura == null) {
+            tipoEstrutura = 1;
+        }
+
+        switch (tipoEstrutura) {
+            case 0: {
+                System.out.println("impossible");
+                break;
+            }
+            case 1: {
+                System.out.println("not sure");
+                break;
+            }
+            case 2: {
+                System.out.println("stack");
+                break;
+            }
+            case 3: {
+                System.out.println("queue");
+                break;
+            }
+            case 4: {
+                System.out.println("priority queue");
+                break;
             }
         }
     }
