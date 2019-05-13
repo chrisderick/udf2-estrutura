feaf7b64e4ecfbb6500bdd18e73cc371c8ed6359
chrisderick <christhyanderick@gmail.com>
2019-05-13 17:00:30 -0300

    Finalização do Projeto Final

--- ProjetoED/src/AdivinhaEstrutura.java
+++ ProjetoED/src/AdivinhaEstrutura.java
@@ -1,8 +1,13 @@
+//Projeto Final de Estrutura de Dados
+//Aluno: Christhyan Derick Dias Barbosa (19828322) e Robson Ribeiro Lucio ()
+//Turma: 2D2
+//Professora: Iasmini Lima
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TODO: Arrumar quando tem inserções após remoções visto que o programa nao reconhece, exemplo de pilha;
+//TODO: Arrumar quando tem insercoes apos remocoes visto que o programa nao reconhece, exemplo de pilha;
 
 public class AdivinhaEstrutura {
 
@@ -25,19 +30,19 @@ public class AdivinhaEstrutura {
 
                 //Confirma que um valor foi informado
                 if (linha.length() > 2) {
-                    //Armazena o comando (inserção(1)/remoção(2)) a ser executado no valor informado;
+                    //Armazena o comando (insercao(1)/remocao(2)) a ser executado no valor informado;
                     Integer valor = Integer.parseInt(linha.substring(2));
                     Integer comando = Integer.parseInt(String.valueOf(linha.charAt(0)));
 
-                    //Verifica se o valor está nos conformes estabelecidos pelo enunciado
+                    //Verifica se o valor esta nos conformes estabelecidos pelo enunciado
                     if (valor >= 0 && valor <= 100) {
-                        //Verifica se está sendo realizada uma inserção ou uma remoção na estrutura
+                        //Verifica se esta sendo realizada uma insercao ou uma remocao na estrutura
                         if (comando == 1) {
                             //vetor[i] = valor;
                             if (valor > maiorValor) maiorValor = valor;
                             insercoes.add(valor);
                         } else if (comando == 2) {
-                            //Confere se o valor a ser removido já foi inserido antes
+                            //Confere se o valor a ser removido ja foi inserido antes
                             if (insercoes.contains(valor)) {
                                 remocoes.add(valor);
                             } else {
@@ -61,13 +66,13 @@ public class AdivinhaEstrutura {
             tipoEstrutura = 0;
         }
 
-        //Comparações
+        //Comparacoes
         //Tipo de Estrutura 1: Not Sure
         if (tipoEstrutura == null) {
             if (tamanho == 1 || tamanho == 2) {
                 tipoEstrutura = 1;
             } else if (tamanho > 3 && insercoes.size() == remocoes.size()) {
-                //Confere se as listas estão invertidas
+                //Confere se as listas estao invertidas
                 boolean estaoInvertidas = true;
                 int cont = remocoes.size()-1;
                 for (Integer valor : insercoes) {
@@ -77,7 +82,7 @@ public class AdivinhaEstrutura {
                     }
                     cont--;
                 }
-                //Confere se as listas são iguais
+                //Confere se as listas sao iguais
                 boolean estaoIguais = true;
                 cont = 0;
                 for (Integer valor : insercoes) {
@@ -87,24 +92,24 @@ public class AdivinhaEstrutura {
                     }
                     cont++;
                 }
-                //Confere se as listas estão ordenadas
+                //Confere se as listas estao ordenadas
                 boolean estaOrdenada = true;
-                //Estrutura é Pilha ou Fila de Prioridade
+                //Estrutura e Pilha ou Fila de Prioridade
                 if (estaoInvertidas) {
-                    //Confere se as inserções estão em ordem crescente
+                    //Confere se as insercoes estao em ordem crescente
                     Integer anterior = insercoes.get(0);
                     for (Integer valor : insercoes) {
-                        //Não considerando valores iguais
+                        //Nao considerando valores iguais
                         if (valor.compareTo(anterior) < 0) {
                             estaOrdenada = false;
                             break;
                         }
                         anterior = valor;
                     }
-                    //Confere se as remoções estão em ordem decrescente
+                    //Confere se as remocoes estao em ordem decrescente
                     anterior = remocoes.get(0);
                     for (Integer valor : remocoes) {
-                        //Não considerando valores iguais
+                        //Nao considerando valores iguais
                         if (valor.compareTo(anterior) > 0) {
                             estaOrdenada = false;
                             break;
@@ -115,22 +120,22 @@ public class AdivinhaEstrutura {
                         tipoEstrutura = 1;
                     }
                 }
-                //Estrutura é Fila normal ou de Prioridade
+                //Estrutura e Fila normal ou de Prioridade
                 else if (estaoIguais) {
-                    //Confere se as inserções estão em ordem decrescente
+                    //Confere se as insercoes estao em ordem decrescente
                     Integer anterior = insercoes.get(0);
                     for (Integer valor : insercoes) {
-                        //Não considerando valores iguais
+                        //Nao considerando valores iguais
                         if (valor.compareTo(anterior) > 0) {
                             estaOrdenada = false;
                             break;
                         }
                         anterior = valor;
                     }
-                    //Confere se as remoções estão em ordem decrescente
+                    //Confere se as remocoes estao em ordem decrescente
                     anterior = remocoes.get(0);
                     for (Integer valor : remocoes) {
-                        //Não considerando valores iguais
+                        //Nao considerando valores iguais
                         if (valor.compareTo(anterior) > 0) {
                             estaOrdenada = false;
                             break;
@@ -142,7 +147,7 @@ public class AdivinhaEstrutura {
                     }
                 }
             }
-            //Alternativa G no Caderno (último comando remove o maior valor do início ou final da estrutura
+            //Alternativa G no Caderno (ultimo comando remove o maior valor do inicio ou final da estrutura
             else if ((maiorValor.equals(insercoes.get(0)) || maiorValor.equals(insercoes.get(insercoes.size()-1))) && remocoes.get(remocoes.size() - 1).equals(maiorValor)) {
                 tipoEstrutura = 1;
             }
@@ -150,12 +155,12 @@ public class AdivinhaEstrutura {
 
         //Tipo de Estrutura 2: Pilha (stack)
         if (tipoEstrutura == null) {
-            //São necessários no mínimo 4 comandos para determinar se a estrutura é uma pilha
+            //Sao necessarios no minimo 4 comandos para determinar se a estrutura e uma pilha
             if (tamanho > 3){
                 boolean pilha = true;
                 int fimInsercoes = insercoes.size()-1;
                 int inicioRemocoes = 0;
-                //Verifica se as remoções feitas estão no padrão de pilha (LIFO)
+                //Verifica se as remocoes feitas estao no padrao de pilha (LIFO)
                 for (int i = remocoes.size()-1; i >= 0; i--){
                     if (!remocoes.get(inicioRemocoes).equals(insercoes.get(fimInsercoes))){
                         pilha = false;
@@ -175,7 +180,7 @@ public class AdivinhaEstrutura {
             boolean fila = true;
             int inicioInsercoes = 0;
             int inicioRemocoes = 0;
-            //Verifica se as remoções feitas estão no padrão de fila (FIFO)
+            //Verifica se as remocoes feitas estao no padrao de fila (FIFO)
             for (int i = remocoes.size() - 1; i >= 0; i--) {
                 if (!remocoes.get(inicioRemocoes).equals(insercoes.get(inicioInsercoes))) {
                     fila = false;
@@ -194,10 +199,10 @@ public class AdivinhaEstrutura {
             //O primeiro valor a ser removido deve ser o maior dos valores inseridos
             if (tamanho > 3 && remocoes.get(0).equals(maiorValor)) {
                 boolean filapri = true;
-                //Verifica se as remoções feitas estão no padrão de fila de prioridade (Remoção dos maiores valores em ordem)
+                //Verifica se as remocoes feitas estao no padrao de fila de prioridade (Remocao dos maiores valores em ordem)
                 int anterior = remocoes.get(0);
                 for (Integer valor : remocoes) {
-                    //Não considerando valores iguais
+                    //Nao considerando valores iguais
                     if (valor.compareTo(anterior) > 0) {
                         filapri = false;
                         break;
@@ -212,13 +217,13 @@ public class AdivinhaEstrutura {
 
         //Tipo 0: Impossible
         if (tipoEstrutura == null) {
-            //Caso o primeiro valor a ser removido não seja o maior dos valores, nem o primeiro ou último a ter sido inserido.
+            //Caso o primeiro valor a ser removido nao seja o maior dos valores, nem o primeiro ou ultimo a ter sido inserido.
             if (tamanho > 3 && !remocoes.get(0).equals(maiorValor) && !remocoes.get(0).equals(insercoes.get(0)) && !remocoes.get(0).equals(insercoes.get(insercoes.size()-1))) {
                 tipoEstrutura = 0;
             }
         }
 
-        //Se nenhuma das comparações definir o valor de tipoEstrutura, esta a define como not sure
+        //Se nenhuma das comparacoes definir o valor de tipoEstrutura, esta a define como not sure
         if (tipoEstrutura == null) {
             tipoEstrutura = 1;
         }
